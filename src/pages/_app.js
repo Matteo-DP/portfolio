e0204be9915ef3a912384eb2c0fe7eb6b9d95ff4
Matteo-DP <matteodepauw@yahoo.com>
2023-01-23 08:49:13 +0100

    fix(SEO): meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,6 +12,10 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <meta name="description" content="I'm a web developer building creative projects. With intermediate experience in both front- and backend development,
+          I'm currently a freelancing full stack developer building projects for fun that make my daily life a bit easier." />
+        <meta name="keywords" content="Matteo De Pauw, Web development, Web developer, Developer, React, NextJS, Next, Matteo, De Pauw" />
+        <meta name="author" content="Matteo De Pauw" />
       </Head>
       <Script src='/smoothScroll.js'></Script>
       <Script src='https://kit.fontawesome.com/2ad3ea3c29.js' crossOrigin='anonymous'></Script>
