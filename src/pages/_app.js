a81fd3927070d2851a98a6bcb8c861f4dd24e630
Matteo-DP <matteodepauw@yahoo.com>
2023-02-05 17:00:23 +0100

    feat(meta): meta tag for google search console verification

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,6 +16,7 @@ export default function App({ Component, pageProps }) {
           I'm currently a freelancing full stack developer building projects for fun that make my daily life a bit easier." />
         <meta name="keywords" content="Matteo De Pauw, Web development, Web developer, Developer, React, NextJS, Next, Matteo, De Pauw" />
         <meta name="author" content="Matteo De Pauw" />
+        <meta name="google-site-verification" content="yHhv87pWy9ZfZdOWB3viM7uxEUleFGUyHMMEU4iypqo" />
       </Head>
       <Script src='/smoothScroll.js'></Script>
       <Script src='https://kit.fontawesome.com/2ad3ea3c29.js' crossOrigin='anonymous'></Script>
